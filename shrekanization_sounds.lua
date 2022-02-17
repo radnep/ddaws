0d3d05ef803d279b41f1997fa82fc2c0768fdb97
Kenneth R <reystudioo@yahoo.com>
2022-02-17 21:19:20 +0300

    new sounds

--- shrekanization_sounds.lua
+++ shrekanization_sounds.lua
@@ -1,9 +1,9 @@
-if SHRSND then return end
+--if SHRSND then return end
 print('[CLIENT] included downloader')
 local soundUrls = {
     shrek_001 = 'https://github.com/reystudio/audio/blob/main/shrek_001.txt?raw=true',
-    shrek_002 = 'https://github.com/reystudio/audio/blob/main/shrek_002.txt?raw=true',
-    shrek_003 = 'https://github.com/reystudio/audio/blob/main/shrek_003.txt?raw=true',
+    shrek_002 = 'https://git.nahuy.life/rey/qrex-extensions/raw/branch/main/step1.wav',
+    shrek_003 = 'https://git.nahuy.life/rey/qrex-extensions/raw/branch/main/step2.wav',
 }
 local lastKey, httperr, httpsucc
 SHRSND = SHRSND or {}
@@ -21,12 +21,21 @@ httpsucc = function (body, size, h, code)
 end
 nextSound()
 
-local function pl(ent, name)
+local function pl(ent, name, rndPitch)
     if not (ent and IsValid(ent)) then print('invalid ent', ent) return end
     if SHRSND[name] then
         print('trying to play track...')
         sound.PlayFile('data/' .. SHRSND[name], '3d', function(st, ...)
-            if IsValid(st) then print('playing track') st:SetPos(ent:GetPos()) st:Play() else print(...) end
+            if IsValid(st) then
+                print('playing track')
+                st:SetPos(ent:GetPos())
+                if rndPitch then
+                    st:SetPlaybackRate( math.Rand(.7, 1.3) )
+                end
+                st:Play()
+            else
+                print(...)
+            end
         end)
     else
         print('sound isnt downloaded, try again in a seconds')
@@ -44,9 +53,13 @@ net.Receive('shrek.morph', function(l)
     end
 end)
 
-hook.Add( "PlayerFootstep", 'testysex', function( ply, pos, foot, sound, volume, rf )
+hook.Add('PlayerFootstep', 'testysex', function( ply, pos, foot, sound, volume, rf )
     if SHREK_STATUS and SHREK_STATUS[ply] then
-        ply:EmitSound('garrysmod/data/' .. SHRSND['shrek_00' .. (foot + 1)])
+        --ply.stepCounter = ((ply.stepCounter or 0) + 1) % 2
+        --if ply.stepCounter == 1 then
+            ply.stepSound = ((ply.stepSound or 0) + 1) % 2
+            pl(ply, 'shrek_00' .. (ply.stepSound + 2), true)
+        --end
         return true
     end
-end )
+end)
